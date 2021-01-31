301df31e95e7ce7332a490a594710b3ab297cf14
Gabriela Dhlamini <madhilam@microsoft.com>
2021-02-01 00:20:34 +0200

    Add project files.

---
+++ EmfuleniDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmfuleniDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
